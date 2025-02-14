ff3030473a4f2e7ec48174eb8167b89847380d1d
Makr <116593190+Makr-Xie@users.noreply.github.com>
2025-02-13 18:17:51 -0600

    commit

---
+++ src/components/GenedPage/GenedPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './GenedPage.css';
+
+const GenedPage = () => {
+    return (
+        <div className="gened-page">
+            <header className="gened-header">
+                <h1>General Education Courses</h1>
+            </header>
+            <main className="gened-content">
+                <section className="gened-courses">
+                    {/* List of General Education Courses */}
+                </section>
+            </main>
+            <footer className="gened-footer">
+                {/* Footer content */}
+            </footer>
+        </div>
+    );
+};
+
+export default GenedPage;
